7f5f26240913dad061fcd6bc5644b4ee158af994
shaikmubasheera <112239781+shaikmubasheera@users.noreply.github.com>
2024-08-01 15:06:41 +0530

    Main Frame
    Created Main frame

--- Expenses/expenses.frontend/src/App.js
+++ Expenses/expenses.frontend/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Try from './Components/Try';
 
 function App() {
   return (
-    'hello there'
+    <Try></Try>
   );
 }
 
